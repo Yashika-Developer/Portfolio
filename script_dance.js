3e602a102ec70ef156e9c88e0de34f6ecbbc7c92
Yashika <139475047+Yashika-Developer@users.noreply.github.com>
2023-08-29 18:49:10 +0530

    Other linking files
    project file/ img/resume

---
+++ script_dance.js
@@ -0,0 +1,51 @@
+gsap.to("#nav",{
+    backgroundColor: "#000",
+  duration: 0.5,
+  height: "100px",
+  scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 1,
+  },
+});
+gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+      trigger: "#main",
+      scroller: "body",
+      start: "top -25%",
+      end: "top -70%",
+      scrub: 2,
+    },
+  });
+
+
+  var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+  
